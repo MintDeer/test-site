b5d25b533b504fdee5a20ec30842de565721f762
MintDeer <54873639+MintDeer@users.noreply.github.com>
2019-09-04 10:19:58 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 let myImage = document.querySelector('img');
-
 myImage.onclick = function() {
 	let mySrc = myImage.getAttribute('src');
 	if(mySrc === 'images/bang-dream.jpg'){
@@ -8,23 +7,19 @@ myImage.onclick = function() {
 		myImage.setAttribute ('src', 'images/bang-dream.jpg');
 	}
 }
-
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
-
 function setUserName() {
 	let myName = prompt('Please enter your name.');
 	localStorage.setItem('name', myName);
 	myHeading.textContent = 'BanG Dream is awesome, ' + myName + '!';
 }
-
 if(!localStorage.getItem('name')) {
 	setUserName();
 } else {
 	let storedName = localStorage.getItem('name');
 	myHeading.textContent = 'Bang Dream is awesome, ' + storedName + '!';
 }
-
 myButton.onclick = function() {
 	setUserName();
 }
